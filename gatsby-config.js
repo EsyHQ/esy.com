32ea485cfb0b1c4ba68dcbf58ce760baadb74e6d
Lemuel Uhuru <sirlemuel@lemuels-mbp.home>
2021-11-09 14:19:10 -0500

    add GA4, update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,40 @@
+
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
+
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://www.Esy.com",
     title: "Esy",
   },
   plugins: [
     "gatsby-plugin-styled-components",
     "gatsby-plugin-image",
     {
-      resolve: "gatsby-plugin-google-analytics",
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingId: "G-XGZRZ2P0V8",
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          `${process.env.GOOGLE_ANALYTICS_TRACKING_ID}`,
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        // gtagConfig: {
+        //   optimize_id: "OPT_CONTAINER_ID",
+        //   anonymize_ip: true,
+        //   cookie_expires: 0,
+        // },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ["/preview/**", "/do-not-track/me/too/"],
+        },
       },
     },
     "gatsby-plugin-react-helmet",
