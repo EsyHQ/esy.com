1facfc49d04382c25b738ad296fb993f7f055cd7
Lemuel Uhuru <sirlemuel@Lemuels-MacBook-Pro.local>
2023-06-11 02:34:39 -0400

    add graphql import to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
-
+import { graphql } from 'gatsby'
 import HomePageComponent from 'src/components/Page/Home/'
-import SEO from '../components/SEO'
+import SEO from 'src/components/SEO'
 
 
 
