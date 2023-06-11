3f2beba2d6e7ec2ff569df7df35ff9630c93cea1
Lemuel Uhuru <sirlemuel@Lemuels-MacBook-Pro.local>
2023-06-11 03:27:34 -0400

    add 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,54 +1,20 @@
 import * as React from "react"
-import { Link } from "gatsby"
+import styled from "styled-components"
+import Layout from "src/components/Layout/container"
 
-// styles
-const pageStyles = {
-  color: "#fff",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
 
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
+export default function NotFoundPage() {
+
+  return (<Layout>
+            <NotFoundPageWrapper>
+              <h1>404: Not Found</h1>
+              <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+            </NotFoundPageWrapper>
+          </Layout>)
 }
 
-// markup
-const NotFoundPage = () => {
-  return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
-  )
-}
 
-export default NotFoundPage
+
+const NotFoundPageWrapper = styled.div`
+  color: #fff;
+`
