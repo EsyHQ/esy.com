11613fc08044c956da3a7fbea0069729d48dc70b
Lem Uhuru <lem@esy.com>
2024-06-17 15:40:42 -0400

    update site brand image

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
     title: "Esy - Your Personal Ai Essay Writer",
     description: `Write your next paper with Esy, an AI assistant that learns your writing style to create original, authentic essays.`,
     twitterUsername: `@esydotcom`,
-    image: "/images/esy-brand-bg.png",
+    image: "/images/esy-brand-bg.jpeg",
     siteUrl: process.env.GATSBY_SITE_URL,
     type: "website",
   },
