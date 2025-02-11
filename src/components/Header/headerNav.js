d7f64495bf9def09fba15c39ddb441fb4c51ef4a
Lem Uhuru <lem@esy.com>
2025-02-11 11:31:38 -0500

    update header nav

--- src/components/Header/headerNav.js
+++ src/components/Header/headerNav.js
@@ -15,7 +15,7 @@ const HeaderNav = ({ activeNav }) => {
               <DesktopNav>
                 {/* <NavItem className={activeNav === 1 ? 'active' : ""}><Link to="/essays">Essays</Link></NavItem>  */}
                 {/* <NavItem className={activeNav === 2 ? 'active' : ""}><Link to="/school">School</Link></NavItem>  */}
-                <NavItem><a target="_blank" href="https://app.esy.com/signup">Sign up</a></NavItem>
+                <NavItem><a target="_blank" href="https://app.esy.com/signup">Log in</a></NavItem>
               </DesktopNav>
 {/*               
               <MobileNav>
@@ -33,16 +33,10 @@ export default HeaderNav
 const HeaderNavContainer = styled.div`
     display: flex;
     justify-content: flex-end;
-    width: 100%;
     color: #fff;
     align-items: center;
-    font-family: 'Spartan',sans-serif;
+    font-family: 'OrigamiIncised',sans-serif;
     font-weight: 500;
-    max-width: 490px;
-    max-width: 225px;
-    // max-width: 135px;
-    max-width: 150px;
-
 `
 
 const DesktopNav = styled.nav`
@@ -68,11 +62,7 @@ const NavItem = styled.div`
   justify-content: flex-end;
   align-items: center;
   position: relative;
-  // margin: 0 20px;
-  // width: 100%;
-  &:last-child {
-    // width: 100%;
-  }
+
 
   &.user-icon {
     width: 100%;
@@ -84,6 +74,10 @@ const NavItem = styled.div`
 
   a {
     text-decoration: none;
+    font-size: 18px;
+    font-family: 'OrigamiIncised',sans-serif !important;
+    letter-spacing: 0.05em;
+
 
     &:hover {
       color: #864ba2;
